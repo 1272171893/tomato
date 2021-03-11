04557f045388822c1536f6243e9cbe5311f0a429
1272171893 <1272171893@qq.com>
2021-03-11 21:06:12 +0800

    demo

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,3 +3,8 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+declare module '*.tsx' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
